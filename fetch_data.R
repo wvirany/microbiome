7cae9bc94d3903d8442aa811ac5218be78d61f32
Walter <106641549+wvirany@users.noreply.github.com>
2023-12-14 17:45:53 -0700

    Add files via upload

---
+++ fetch_data.R
@@ -0,0 +1,27 @@
+# Loading all libraries:
+
+library(curatedMetagenomicData)
+library(curatedMetagenomicAnalyses)
+library(dplyr)
+
+# Grabbing data from curateMetagenomicData()
+
+healthy_hmp <- sampleMetadata |>
+  filter(study_name == "HMP_2019_ibdmdb") |>
+  filter(disease == "healthy") |>
+  select(where(~ !any(is.na(.x)))) |>
+  returnSamples("relative_abundance", rownames="short", counts=TRUE)
+
+ibd_hmp <- sampleMetadata |>
+  filter(study_name == "HMP_2019_ibdmdb") |>
+  filter(disease == "IBD") |>
+  select(where(~ !any(is.na(.x)))) |>
+  returnSamples("relative_abundance", rownames="short", counts=TRUE)
+
+ibd <- t(ibd_hmp@assays@data@listData[["relative_abundance"]])
+
+healthy <- t(healthy_hmp@assays@data@listData[["relative_abundance"]])
+
+write.csv(ibd, "~/Code/R/curatedMetagenomicData/ibd_rel_abundance.csv", row.names=FALSE)
+
+write.csv(healthy, "~/Code/R/curatedMetagenomicData/healthy_rel_abundance.csv", row.names=FALSE)
